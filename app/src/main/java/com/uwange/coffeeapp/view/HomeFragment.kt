3253179a487d4630169d9a3c69ffbdd77d8ccb17
park-cy <cy_park@pntbiz.com>
2023-06-05 21:07:22 +0900

    [update]ViewPager2의 우하단에 Counter 제작

--- app/src/main/java/com/uwange/coffeeapp/view/HomeFragment.kt
+++ app/src/main/java/com/uwange/coffeeapp/view/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.gms.tasks.Task
+import com.uwange.coffeeapp.R
 import com.uwange.coffeeapp.adapter.ImageSliderAdapter
 import com.uwange.coffeeapp.databinding.FragmentHomeBinding
 import com.uwange.coffeeapp.viewmodel.HomeFragmentViewModel
@@ -45,8 +47,20 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+    //TODO::차후 infinite pager adapter, auto slider 제작
     private fun setupImageSlider(imageUrls: List<Task<Uri>>) {
         val imageSliderAdapter = ImageSliderAdapter(requireContext(), imageUrls)
         binding.vpImageSlider.adapter = imageSliderAdapter
+        setupViewPageCounter(imageUrls.size)
+    }
+
+    private fun setupViewPageCounter(itemSize: Int) {
+        binding.tvViewPagerCounter.text = getString(R.string.viewpager2_banner, 1, itemSize)
+        binding.vpImageSlider.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tvViewPagerCounter.text = getString(R.string.viewpager2_banner, position + 1, itemSize)
+            }
+        })
     }
 }
